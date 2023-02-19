e2334b441033e197861ef930d570730806803db7
LeonFroelje <leon.froelje@ewetel.net>
2023-02-19 19:58:12 +0100

    jedash

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,23 @@
+use dmx::{self, DmxTransmitter};
+use std::{thread, time};
+mod universe;
+mod fixture;
+mod channel;
+use crate::universe::Universe;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut dmx_port = dmx::open_serial("/dev/ttyAMA0").unwrap();
+
+    // a typical 4-channel device, expecting RGBV values. this will set a
+    // fairly bright yellow.
+    let data = &[0xff; 512];
+ 
+    loop {
+        dmx_port.send_dmx_packet(data).unwrap();
+ 
+        // repeat about every 51 ms. for more accurate frame timings,
+        // consider using the ticktock crate.
+        thread::sleep(time::Duration::new(0, 50_000_000));
+    }
 }
