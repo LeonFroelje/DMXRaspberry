b1a9fed57fd88f1eca7cc5c759ab587ab0f40346
LeonFroelje <leonfroelje3@gmail.com>
2023-04-07 23:39:34 +0200

    lol

--- server/src/main.rs
+++ server/src/main.rs
@@ -47,26 +47,26 @@ async fn main() -> std::io::Result<()> {
     let default_port = cnf.port().clone();
     let default_universe = cnf.default_universe().clone();
     // connect to db
-    let client = create_mongo_client().await;
-    let db = client.database("dmxraspberry");
-    let collections = db.list_collection_names(None)
-        .await
-        .unwrap();
-    let missing_collections = db_collections.iter()
-        .filter(|s| !collections.contains(s));
-    // create missing collections
-    for collection in missing_collections{
-        log::info!("{}", collection);
-        match db.create_collection(collection, None).await{
-            Ok(_) => {},
-            Err(e) => panic!("{}", e)
-        };
-        if collection == "universes"{
-            let universes: Collection<Universe> = db.collection(&collection);
-            let universe: Universe = serde_json::from_str(&fs::read_to_string(format!("./Universes/{default_universe}.json")).unwrap()).unwrap();
-            universes.insert_one(universe, None).await.unwrap();
-        }
-    }
+    // let client = create_mongo_client().await;
+    // let db = client.database("dmxraspberry");
+    // let collections = db.list_collection_names(None)
+    //     .await
+    //     .unwrap();
+    // let missing_collections = db_collections.iter()
+    //     .filter(|s| !collections.contains(s));
+    // // create missing collections
+    // for collection in missing_collections{
+    //     log::info!("{}", collection);
+    //     match db.create_collection(collection, None).await{
+    //         Ok(_) => {},
+    //         Err(e) => panic!("{}", e)
+    //     };
+    //     if collection == "universes"{
+    //         let universes: Collection<Universe> = db.collection(&collection);
+    //         let universe: Universe = serde_json::from_str(&fs::read_to_string(format!("./Universes/{default_universe}.json")).unwrap()).unwrap();
+    //         universes.insert_one(universe, None).await.unwrap();
+    //     }
+    // }
     // Server used for websocket communication and providing an interface between the routes
     // and the DMX runtime
     let rt_server = RtServer::new().start();
@@ -94,7 +94,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(cors)
             .app_data(web::Data::new(rt_server.clone()))
-            .app_data(web::Data::new(db.clone()))
+            // .app_data(web::Data::new(db.clone()))
             .service(
                 web::scope("/api")
                 .service(web::resource("/ws").route(web::get().to(websocket::create_socket)))
