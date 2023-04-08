5e7646deee4b8d3e20481e0a7fe97c170dec7ae7
LeonFroelje <leonfroelje3@gmail.com>
2023-04-08 15:50:27 +0200

    lel

--- server/src/actors/websockets/datasocket.rs
+++ server/src/actors/websockets/datasocket.rs
@@ -78,7 +78,7 @@ impl DataSocket{
     fn handle_text_message(&self, message: TextMessage, ctx: &mut <DataSocket as Actor>::Context){
         log::info!("Text message angekommen");
         match message.url{
-            String::from("/fixtures/update") => {
+            {String::from("/fixtures/update")} => {
                 let fixture: Result<Fixture, Error> = serde_json::from_str(&message.text);
                 match fixture {
                     Ok(fixture) => {
@@ -89,7 +89,7 @@ impl DataSocket{
                     }
                 }
             },
-            String::from("/fixtures/add") => {
+            {String::from("/fixtures/add")} => {
                 let fixture: Result<Fixture, Error> = serde_json::from_str(&message.text);
                 match fixture {
                     Ok(fixture) => {
@@ -100,7 +100,7 @@ impl DataSocket{
                     }
                 }
             }
-            String::from("/fixtures/remove") => {
+            {String::from("/fixtures/remove")} => {
                 let fixture: Result<Fixture, Error> = serde_json::from_str(&message.text);
                 match fixture {
                     Ok(fixture) => {
