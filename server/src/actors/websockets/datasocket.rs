e646fa377a706aa17e2955ec7f3c5e460f79fa8d
LeonFroelje <leonfroelje3@gmail.com>
2023-04-08 17:14:09 +0200

    lel

--- server/src/actors/websockets/datasocket.rs
+++ server/src/actors/websockets/datasocket.rs
@@ -152,7 +152,6 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for DataSocket{
             }
 
             ws::Message::Text(text) =>  {
-                log::info!("{:?}", text);
                 let message: TextMessage = serde_json::from_str(&text).unwrap();
                 // match for routes
                 self.handle_text_message(message, ctx);
